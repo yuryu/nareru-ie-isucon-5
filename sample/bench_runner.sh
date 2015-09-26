831336ecaa368531c1bbd445469620d68d8911a4
Haruka Iwao <yuryu@yuryu.jp>
2015-09-26 10:24:43 +0900

    Update benchmark runner sample to include git repo status and benchmark log

--- sample/bench_runner.sh
+++ sample/bench_runner.sh
@@ -1,5 +1,5 @@
-rm -f /var/log/nginx/*.log
-rm -f /var/log/mysql/*.log
+# rm -f /var/log/nginx/*.log
+# rm -f /var/log/mysql/*.log
 
 # restartさせるやつ書き足す
 sudo service mysqld restart
@@ -7,10 +7,15 @@ sudo service nginx restart
 sudo service redis restart
 # sudo service varnish restart
 
-./benchmarker # sample
-
 c_time=`date '+%H-%M'`
-LOG_DIR=~/log 
-mkdir -p LOG_DIR/${c_time}
-sudo mv /var/log/nginx/access.log LOG_DIR/${c_time}/nginx_access.log
-sudo mv /var/log/mysql/slow.log LOG_DIR/${c_time}/mysql_slow.log
+LOG_BASE=~/log
+LOG_DIR=$LOG_DIR/${c_time}
+mkdir -p $LOG_DIR
+
+# sample
+# git status | tee $LOG_DIR/benchmark.log
+# git diff | tee $LOG_DIR/benchmark.log
+# ./benchmarker | tee $LOG_DIR/benchmark.log
+
+sudo cp /var/log/nginx/*.log $LOG_DIR/nginx_access.log
+sudo cp /var/log/mysql/*.log $LOG_DIR/mysql_slow.log
