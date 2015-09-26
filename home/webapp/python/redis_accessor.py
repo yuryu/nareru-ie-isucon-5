fe8b31f9c56af3fcef59ff3c9adc8c9181fb69ac
cocodrips <cocodrips@gmail.com>
2015-09-26 16:48:06 +0900

    Add friend counter.

--- home/webapp/python/redis_accessor.py
+++ home/webapp/python/redis_accessor.py
@@ -1,7 +1,6 @@
 import redis
 
-p_step_user="step_user_{user_id}"
-p_friend_counter="friend_counter_{}"
+p_step_user="step_user_{user_id}" # list
 
 conn = redis.StrictRedis(host='localhost', port=6379)
 def get_user_step(conn, user_id):
@@ -13,3 +12,12 @@ def set_user_step(conn, user_id, step_user):
         conn.lpop(p_step_user.format(user_id=user_id))
     conn.rpush(p_step_user.format(user_id=user_id), step_user)
 
+
+p_friend_counter="friend_counter_{user_id}" # string (int)
+
+def get_user_frind_num(conn, user_id):
+    cnt = conn.get(p_friend_counter.format(user_id=user_id))
+    return int(cnt) if cnt else 0
+
+def inc_user_frind_num(conn, user_id):
+    conn.incr(p_friend_counter.format(user_id=user_id))
